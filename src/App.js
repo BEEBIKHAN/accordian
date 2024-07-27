0ece0bab8faeb22a9b8d042db976b3161bd39e69
BEEBI_HAJRAH MOHAMED_KHAN <beebikhan4@gmail.com>
2024-07-27 21:09:39 +0200

    initilize

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Accordian from './Components';
+
+function App() {
+  return (
+    <div className="App">
+     <Accordian/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+    id : '1',
+    question: "What is React?",
+    answer: "React is a popular JavaScript library for building user interfaces, particularly single-page applications where you need a fast, interactive, and dynamic experience. "
+    },
+    {
+    id : '2',
+    question: "What is JavaScript?",
+    answer: "JavaScript is a high-level, dynamic, and interpreted programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.   "
+    },
+    {
+    id : '3',
+    question: "What is Jsx?",
+    answer: "JSX is a syntax extension for JavaScript that looks similar to HTML. It allows you to write HTML-like code within JavaScript, making it easier to create and understand the structure of your components. "
+    },
+    {
+    id : '4',
+    question: "What is JSON?",
+    answer: "JSON (JavaScript Object Notation) is a lightweight data-interchange format that is easy for humans to read and write, and easy for machines to parse and generate. It is widely used for transmitting data between a server and a web application, as well as for configuration files, data storage, and more. "
+    },
+    {
+    id : '5',
+    question: "What is DOM?",
+    answer: "The Document Object Model (DOM) is a programming interface for web documents. It represents the structure of a document as a tree of objects, allowing programs and scripts to dynamically access and update the content, structure, and style of web pages. "
+    },
+    ];
+    export default data;
